2c5ef14f9ceb61e9b34b32be7e1a0307cfbb567e
Wellington Junio <wjuniossantos009@gmail.com>
2025-02-03 18:47:23 -0300

    Commit ex 05

--- src/main/java/EX_05_Java/Ex_05.java
+++ src/main/java/EX_05_Java/Ex_05.java
@@ -30,10 +30,71 @@ public class Ex_05 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite sua idade: ");
         int idade = sc.nextInt();
-        if (idade <= 12) {
-
+        if (idade <= 10) {
+            System.out.println("Sua faixa de classificação é A");
+        } else if (idade >= 11 && idade <= 20) {
+            System.out.println("Sua faixa de classificação é B");
+        } else if (idade >= 21 && idade <= 30) {
+            System.out.println("Sua faixa de classificação é C");
+        } else {
+            System.out.println("Digite um valor válido.");
         }
     }
+
+    public static void EX03() {
+        System.out.println("Questão 3: Receba a altura de uma pessoa e determine se ela é considerada baixa,\n" +
+                "média ou alta.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a sua altura: ");
+        float altura = sc.nextByte();
+
+        if (altura >= 1.50 && altura <= 1.60) {
+            System.out.println("Sua altura é considerada baixa.");
+        } else if (altura >= 1.61 && altura <= 1.79) {
+            System.out.println("Sua altura é considerada média.");
+        } else if (altura >= 1.80) {
+            System.out.println("Sua altura é considerada alta.");
+        } else {
+            System.out.println("Digite um valor válido.");
+        }
+    }
+
+    public static void EX04() {
+        System.out.println("");
+        System.out.println("Questão 4: Verifique o nível de risco de uma pessoa baseado na pressão arterial\n" +
+                "(normal, moderado ou alto).");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a sua pressão arterial: ");
+        float pressao = sc.nextByte();
+
+        if (pressao <= 12.8) {
+            System.out.println("Sua pressão arterial está boa, risco normal ou moderado.");
+        } else if (pressao >= 14) {
+            System.out.println("Sua pressão está alta, risco alto.");
+        } else if (pressao <= 9) {
+            System.out.println("Sua pressão está baixa, risco alto.");
+        }
+    }
+
+    public static void EX05() {
+        System.out.println("Questão 5: Receba a temperatura em graus Celsius e indique se está frio, agradável\n" +
+                "ou quente.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite a temperatura em Celsius: ");
+        float temp = sc.nextByte();
+
+        if (temp < 19) {
+            System.out.println(temp + "°");
+            System.out.println("O clima está frio");
+        } else if (temp >= 20 && temp <= 26) {
+            System.out.println(temp + "°");
+            System.out.println("O clima está agradável.");
+        } else if (temp > 26) {
+            System.out.println(temp + "°");
+            System.out.println("O clima está quente.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int questao;
@@ -57,17 +118,17 @@ public class Ex_05 {
                     EX01();
                     break;
                 case 2:
-
-
+                    EX02();
+                    break;
                 case 3:
-
-
+                    EX03();
+                    break;
                 case 4:
-
-
+                    EX04();
+                    break;
                 case 5:
-
-
+                    EX05();
+                    break;
                 case 0:
                     System.out.println("Obrigado por acessar!");
                     break;
@@ -81,4 +142,3 @@ public class Ex_05 {
         sc.close();
     }
 }
-
